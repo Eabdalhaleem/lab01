6fcbc05b6802ee14e0593cc288307eb6b484c75e
Eabdalhaleem <eattaamneh@yahoo.com>
2021-06-20 22:47:15 +0300

    solving lab01

---
+++ reactlab01/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Footers extends React.Component{
+ render(){
+     return(
+         <h5> copyright 2021 </h5>
+     )
+ }
+    
+     
+
+
+}
+export default Footers
---
+++ reactlab01/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Headers extends React.Component{
+ render(){
+     return(
+         <h1>HornedBeasts</h1>
+     )
+ }
+    
+     
+
+
+}
+export default Headers
